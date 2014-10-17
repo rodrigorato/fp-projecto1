1765f084ea3310082a7fc89973743442c65633f0
rodrigorato <rodrigorato1996@hotmail.com>
2014-10-17 22:45:09 +0100

    Milestone 1 - Comentários actualizados
    Foram actualizados os comentários. Outra vez.

--- cartoes.py
+++ cartoes.py
@@ -29,9 +29,9 @@ V = (("4024", "4532", "4556"), (13, 16), "Visa")
 
 def calc_soma(cadeia):
     '''
-	Recebe uma string e devolve o inteiro correspondente 'a 
-	soma ponderada dos digitos, calculada com o algoritmo de Luhn.
-	'''
+    Recebe uma string e devolve o inteiro correspondente 'a 
+    soma ponderada dos digitos, calculada com o algoritmo de Luhn.
+    '''
     cadeia = inverte_string(cadeia)
     cadeia = duplicaImparesESubtraiNove(cadeia)
     soma = 0
@@ -41,9 +41,9 @@ def calc_soma(cadeia):
 
 def inverte_string(cadeia):
     '''
-	Recebe uma string e devolve a string correspondente 'a 
-	primeira invertida, necessario para o algoritmo de Luhn.
-	'''
+    Recebe uma string e devolve a string correspondente 'a 
+    primeira invertida, necessario para o algoritmo de Luhn.
+    '''
     cadeia_invertida = ""
     n = len(cadeia) - 1
     while n >= 0:
@@ -53,12 +53,12 @@ def inverte_string(cadeia):
 
 def duplicaImparesESubtraiNove(cadeia):
     '''
-	Recebe uma string e devolve a string com os numeros em 
-	indices impares multiplicados por dois e caso esses numeros
-	sejam superiores a 9 e' lhes subtraido o valor 9. 
-	Esta string tera' de ser uma em que para qualquer que seja 
-	o numero inteiro n, eval(cadeia[n]) existe e e' um numero inteiro.
-	'''
+    Recebe uma string e devolve a string com os numeros em 
+    indices impares multiplicados por dois e caso esses numeros
+    sejam superiores a 9 e' lhes subtraido o valor 9. 
+    Esta string tera' de ser uma em que para qualquer que seja 
+    o numero inteiro n, eval(cadeia[n]) existe e e' um numero inteiro.
+    '''
     duplicados = ""
     n = 0
     for n in range(len(cadeia)):
@@ -73,10 +73,10 @@ def duplicaImparesESubtraiNove(cadeia):
 
 def luhn_verifica(str_nr_cartao):
     '''
-	Recebe uma string (numero do cartao) e devolve o valor 
-	logico True se o numero do cartao passar o algoritmo de Luhn
-	e False em caso contrario.
-	'''
+    Recebe uma string (numero do cartao) e devolve o valor 
+    logico True se o numero do cartao passar o algoritmo de Luhn
+    e False em caso contrario.
+    '''
     digito_verificacao = eval(str_nr_cartao) % 10
     str_nr_sem_verificacao = str(eval(str_nr_cartao) // 10)
     cartao_somado = calc_soma(str_nr_sem_verificacao) + digito_verificacao
@@ -84,9 +84,9 @@ def luhn_verifica(str_nr_cartao):
 
 def comeca_por(cad1, cad2):
     '''
-	Recebe duas cadeias e verifica se a segunda esta'
-	contida no inicio da primeira. Devolve True/False.
-	'''
+    Recebe duas cadeias e verifica se a segunda esta'
+    contida no inicio da primeira. Devolve True/False.
+    '''
     if len(cad2) > len(cad1) or cad2 == "":
         return False
     for n in range(len(cad2)):
@@ -96,10 +96,10 @@ def comeca_por(cad1, cad2):
 
 def comeca_por_um(cad, t_cads):
     '''
-	Recebe uma cadeia e um tuplo de cadeias e 
-	verifica se alguma das cadeias no tuplo esta' 
-	contida no inicio da primeira cadeia. Devolve True/False
-	'''
+    Recebe uma cadeia e um tuplo de cadeias e 
+    verifica se alguma das cadeias no tuplo esta' 
+    contida no inicio da primeira cadeia. Devolve True/False
+    '''
     for elemento in t_cads:
         if comeca_por(cad, elemento):
             return True
@@ -107,10 +107,10 @@ def comeca_por_um(cad, t_cads):
 
 def valida_iin(cadeia):
     '''
-	Usa os tuplos definidos no inicio do ficheiro para 
-	validar o iin da cadeia que a funcao recebe.Recebe o 
-	numero de um cartao e devolve o nome da rede emissora em string.
-	'''    
+    Usa os tuplos definidos no inicio do ficheiro para 
+    validar o iin da cadeia que a funcao recebe.Recebe o 
+    numero de um cartao e devolve o nome da rede emissora em string.
+    '''    
     if comeca_por_um(cadeia, AE[0]) and numero_digitos(cadeia, AE[1]): 
         return AE[2]
     if comeca_por_um(cadeia, DCI[0]) and numero_digitos(cadeia, DCI[1]):
@@ -129,9 +129,9 @@ def valida_iin(cadeia):
 
 def numero_digitos(cad1, tup1):
     '''
-	Recebe uma cadeia e um tuplo de inteiros e verifica 
-	se o comprimento da cadeia e' igual a algum dos inteiros no tuplo.
-	'''
+    Recebe uma cadeia e um tuplo de inteiros e verifica 
+    se o comprimento da cadeia e' igual a algum dos inteiros no tuplo.
+    '''
     for i in range(len(tup1)):
         if len(cad1) == tup1[i]:
             return True
@@ -139,11 +139,11 @@ def numero_digitos(cad1, tup1):
 
 def categoria(cadeia):
     '''
-	Recebe uma cadeia e verifica o se o que esta' no indice 0 
-	dessa cadeia e' alguma categoria conhecida e devolve a cadeia 
-	correspondente 'a categoria. Em caso contrario devolve a cadeia 
-	de caracteres vazia.
-	'''
+    Recebe uma cadeia e verifica o se o que esta' no indice 0 
+    dessa cadeia e' alguma categoria conhecida e devolve a cadeia 
+    correspondente 'a categoria. Em caso contrario devolve a cadeia 
+    de caracteres vazia.
+    '''
     if cadeia[0] == "1":
         return "Companhias aereas"
     if cadeia[0] == "2":
@@ -166,13 +166,13 @@ def categoria(cadeia):
 
 def verifica_cc(numero_cartao):
     '''
-	Recebe um numero de um cartao e verifica se e' valido, 
-	ou seja: e' verificado pelo algoritmo de Luhn, tem uma
-	categoria valida associada ao mesmo e o seu iin pode ser validado.
-	Devolve o tuplo correspondente as strings da categoria e do
-	nome da rede emissora. Em caso de um cartao inva'lido devolve 
-	a cadeia de caracteres "cartao invalido".
-	'''
+    Recebe um numero de um cartao e verifica se e' valido, 
+    ou seja: e' verificado pelo algoritmo de Luhn, tem uma
+    categoria valida associada ao mesmo e o seu iin pode ser validado.
+    Devolve o tuplo correspondente as strings da categoria e do
+    nome da rede emissora. Em caso de um cartao inva'lido devolve 
+    a cadeia de caracteres "cartao invalido".
+    '''
     cc_cartao = str(numero_cartao)
     if luhn_verifica(cc_cartao) and categoria(cc_cartao) != "" and valida_iin(cc_cartao) != "":
         return (categoria(cc_cartao), valida_iin(cc_cartao))
@@ -183,17 +183,17 @@ def verifica_cc(numero_cartao):
 
 def randomCartaoSemVerificacao(abrev):
     '''
-	Recebe abreviatura da rede emissora, se for 
-	valida gera o numero aleatoriamente pela funcao
-	encheRandomAte para a qual passa como argumentos 
-	o digito inicial (ou digitos iniciais, se for o caso
-	usa escolheElementoTuplo para escolher um prefixo
-	aleatoriamente) e o numero de digitos possiveis menos
-	um, o digito de verificacao. Em caso de diferentes 
-	"comprimentos" possiveis escolhe do tuplo dos comprimentos
-	um aleatoriamente, como para os prefixos. Devolve o
-	inteiro do numero do cartao sem dig. de verificacao.
-	'''
+    Recebe abreviatura da rede emissora, se for 
+    valida gera o numero aleatoriamente pela funcao
+    encheRandomAte para a qual passa como argumentos 
+    o digito inicial (ou digitos iniciais, se for o caso
+    usa escolheElementoTuplo para escolher um prefixo
+    aleatoriamente) e o numero de digitos possiveis menos
+    um, o digito de verificacao. Em caso de diferentes 
+    "comprimentos" possiveis escolhe do tuplo dos comprimentos
+    um aleatoriamente, como para os prefixos. Devolve o
+    inteiro do numero do cartao sem dig. de verificacao.
+    '''
     num = 0
     if abrev == "AE":
         num = encheRandomAte(escolheElementoTuplo(AE[0]), AE[1][0] - 1)
@@ -213,10 +213,10 @@ def randomCartaoSemVerificacao(abrev):
 
 def tuploInteirosTuploCadeias(tuplo_ints):
     '''
-	Recebe um tuplo com membros do tipo 
-	inteiro e devolve um tuplo com as cadeias 
-	de caracteres correspondentes a esses inteiros.
-	'''
+    Recebe um tuplo com membros do tipo 
+    inteiro e devolve um tuplo com as cadeias 
+    de caracteres correspondentes a esses inteiros.
+    '''
     tuplo_cadeias = ()
     for i in tuplo_ints: # Nao se usa range() para evitar problemas, e' mais facil de obter a string correspondente ao inteiro deste modo.
         tuplo_cadeias = tuplo_cadeias + (str(i),)
@@ -224,11 +224,11 @@ def tuploInteirosTuploCadeias(tuplo_ints):
 
 def gera_num_cc(abrev):
     '''
-	Gera um numero de cartao aleatorio dada a 
-	abreviatura da rede emissora como parametro, 
-	devolve um numero de um cartao valido (inteiro) 
-	que pertence 'a rede desejada.
-	'''
+    Gera um numero de cartao aleatorio dada a 
+    abreviatura da rede emissora como parametro, 
+    devolve um numero de um cartao valido (inteiro) 
+    que pertence 'a rede desejada.
+    '''
     num = 0
     num = randomCartaoSemVerificacao(abrev)
     num = (num * 10) + eval(digito_verificacao(str(num)))
@@ -236,28 +236,28 @@ def gera_num_cc(abrev):
         
 def encheRandomAte(numero, comprimento):
     '''
-	Devolve um inteiro "completado", aleatoriamente, com 
-	prefixo de valor igual 'a variavel numero, ate' o seu 
-	comprimento ser igual ao valor do inteiro comprimento.
-	'''
+    Devolve um inteiro "completado", aleatoriamente, com 
+    prefixo de valor igual 'a variavel numero, ate' o seu 
+    comprimento ser igual ao valor do inteiro comprimento.
+    '''
     while len(str(numero)) < comprimento: # len(str(numero)) da' o "comprimento" do numero a "preencher".
         numero = (numero * 10) + randomZeroANum(9)
     return numero
 
 def randomZeroANum(num):
     '''
-	Escolhe um inteiro aleatoriamente desde 0,
-	ate' o valor da variavel/parametro num. E'
-	possivel obter tanto o numero 0 como o valor de num.
-	'''
+    Escolhe um inteiro aleatoriamente desde 0,
+    ate' o valor da variavel/parametro num. E'
+    possivel obter tanto o numero 0 como o valor de num.
+    '''
     return int(random.random() * (num + 1)) # int() faz com que so seja devolvida a parte inteira do numero gerado
 
 def escolheElementoTuplo(tuplo_de_cadeias):
     '''
-	Escolhe aleatoriamente um elemento de um tuplo cujos
-	elementos sao strings e devolve o inteiro correspondente 
-	a esse elemento.
-	'''
+    Escolhe aleatoriamente um elemento de um tuplo cujos
+    elementos sao strings e devolve o inteiro correspondente 
+    a esse elemento.
+    '''
     tuplo_de_inteiros = ()
     for i in tuplo_de_cadeias: # aqui define-se todos os elementos de um tuplo_de_inteiros, que sao os inteiros correspondentes 'as strings do primeiro tuplo.
         tuplo_de_inteiros = tuplo_de_inteiros + (eval(i),)
@@ -265,10 +265,10 @@ def escolheElementoTuplo(tuplo_de_cadeias):
     
 def digito_verificacao(cc_cartao_sem_ultimo):
     '''
-	Calcula o digito de verificacao de uma cadeia de 
-	caracteres correspondente a um numero de um cartao
-	sem este ultimo digito. Devolve apenas o ultimo digito.
-	'''
+    Calcula o digito de verificacao de uma cadeia de 
+    caracteres correspondente a um numero de um cartao
+    sem este ultimo digito. Devolve apenas o ultimo digito.
+    '''
     soma_sem_ultimo = calc_soma(cc_cartao_sem_ultimo)
     if soma_sem_ultimo % 10 == 0:
         return "0"
